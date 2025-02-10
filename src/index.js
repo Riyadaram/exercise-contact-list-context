29b4b49686749bfdc01e75b1c1f6351641d104f9
Mohammed El Mangadi <138013464+Riyadaram@users.noreply.github.com>
2025-02-10 04:50:36 +0000

    First Commit

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import {createRoot} from 'react-dom/client'
+
+import "../styles/index.css";
+
+import Layout from './layout.js'
+
+const root = createRoot(document.querySelector("#app"))
+
+root.render(<Layout/>)
+
+console.log('Hola Mundo!')
